aa76542d17b39eaa9948f87a84a6a6c584411a17
Cristian Mendoza <ccamilo.mendoza@udea.edu.co>
2020-02-14 22:28:06 -0500

    Seller name

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import ItemPrice from '../ItemPrice/ItemPrice';
 import './Item.css';
 const Item = ({ item }) => {
+  const [loadingSeller, setLoadingSeller] = useState(true);
+  const [sellerName, setSellerName] = useState('');
+  const fetchSeller = useCallback(async () => {
+    const response = await (
+      await fetch(
+        `https://api.mercadolibre.com/sites/MCO/search?seller_id=${item.seller.id}`
+      )
+    ).json();
+    setSellerName(response.seller.nickname);
+    setLoadingSeller(false);
+  }, [item.seller.id]);
+
+  useEffect(() => {
+    fetchSeller();
+  }, [fetchSeller]);
+
   return (
     <div className="item-container">
       <div className="card-header">
@@ -24,7 +40,7 @@ const Item = ({ item }) => {
         {item.shipping.free_shipping && (
           <p className="free-shiping">Envío gratis</p>
         )}
-        Vendedor: {item.seller.id}
+        {!loadingSeller && <p>{sellerName}</p>}
       </div>
     </div>
   );
